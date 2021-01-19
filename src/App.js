2aa75f0335debb5c079a69a75a834934b5f672f9
Devansh Dubey <devanshdubey2001@gmail.com>
2021-01-20 00:21:53 +0530

    basic structure setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="green"></div>
+			<div className="app__body">
+				<Sidebar />
+				<div className="test"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBo3ivq5XFuUDfCd8YRjBaAOU9sIriU1i0",
+    authDomain: "whatsapp-clone0110.firebaseapp.com",
+    projectId: "whatsapp-clone0110",
+    storageBucket: "whatsapp-clone0110.appspot.com",
+    messagingSenderId: "390287830314",
+    appId: "1:390287830314:web:e960c46b4bb7dfc3510253",
+    measurementId: "G-5RMJ046G7M"
+};
